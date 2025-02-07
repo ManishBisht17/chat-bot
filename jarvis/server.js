e171743202adaad39d135f19f16cb9a160a585ec
Manish Singh Bisht <manisbst123@gmail.com>
2025-02-08 02:39:17 +0530

    readme

--- jarvis/server.js
+++ jarvis/server.js
@@ -1,12 +1,10 @@
-import app from './app.js';
-import dotenv from 'dotenv';
+import app from "./app.js";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
 
 const server = app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
-
-
